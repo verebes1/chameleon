ec963178dd6ea737603c0be70ff5813352d7c550
David <dverbenyi@gmail.com>
2022-09-18 23:08:09 +0100

    Updated to Swift 5

--- ChameleonDemo/AppDelegate.swift
+++ ChameleonDemo/AppDelegate.swift
@@ -14,10 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 
-        Chameleon.setGlobalThemeUsingPrimaryColor(.flatPlum,
-            withSecondaryColor: .flatBlue,
+        Chameleon.setGlobalThemeUsingPrimaryColor(.flatPlum(),
+                                                  withSecondaryColor: .flatBlue(),
             andContentStyle: .contrast)
         
         return true
